21bd0d0de8c1ef7b2f071e2189e9cf73e1795b56
seenujan <seenujanuthayanan@gmail.com>
2026-01-26 10:48:59 +0530

    forgot password

--- edubridge/backend/scripts/check_env.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
-
-console.log('--- Environment Check ---');
-console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
-console.log('Expected:', 'http://localhost:3000');
-console.log('-------------------------');
